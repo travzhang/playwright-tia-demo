89d85544d75f388c84cf8dcffad4b1dce2e27b52
Travis Zhang <wr.zhang25@gmail.com>
2026-08-20 14:53:47 +0800

    feat: update

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import istanbul from "vite-plugin-istanbul";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  // @ts-ignore
+  plugins: [react(),istanbul({
+    forceBuildInstrument: true,
+  })],
 });
